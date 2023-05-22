41d5d034e372abeea25b8ad74f0063ffd15998f1
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2023-05-22 15:17:50 +0200

    Trying to fix css

--- www/js/index.js
+++ www/js/index.js
@@ -165,93 +165,94 @@ function onError() {
       main.appendChild(cardList);
   
       for (var i = 0; i < data.length; i++) {
-      var article = document.createElement('article');
-      article.className = 'active';
-      article.setAttribute('data-id', data[i].id);
-  
-      var link = document.createElement('a');
-      link.href = 'https://navlab.ericroy.net/server/' + data[i].id + '/edit';
-  
-      var heading = document.createElement('h2');
-      heading.className = 'p-2';
-      heading.textContent = data[i].name;
-  
-      var div1 = document.createElement('div');
-      div1.className = 'flex';
-  
-      var div2 = document.createElement('div');
-      div2.className = 'w-8/12';
-  
-      var span1 = document.createElement('span');
-      span1.className = 'state-off text-red-500 text-xl';
-      span1.innerHTML = '<i class="fa fa-times"></i> Off';
-  
-      var span2 = document.createElement('span');
-      span2.className = 'state-processing text-gray-500 text-xl';
-      span2.innerHTML = '<i class="fa fa-spinner animate-spin"></i> ...';
-  
-      var span3 = document.createElement('span');
-      span3.className = 'state-on text-green-500 text-xl';
-      span3.innerHTML = '<i class="fa fa-check"></i> ' + data[i].status;
-  
-      var br1 = document.createElement('br');
-      var br2 = document.createElement('br');
-  
-      var button1 = document.createElement('button');
-      button1.className = 'start-button bg-green-400 hover:bg-green-600 p-2 rounded text-white status-off status-button';
-      button1.innerHTML = '<i class="fa fa-power-off"></i> Start';
-  
-      var button2 = document.createElement('button');
-      button2.className = 'cancel-button bg-gray-400 hover:bg-gray-600 p-2 rounded text-white status-off status-button';
-      button2.innerHTML = '<i class="fa fa-spinner animate-spin"></i> Cancel';
-  
-      var button3 = document.createElement('button');
-      button3.className = 'processing-button bg-gray-400 p-2 rounded text-white status-off status-button cursor-not-allowed';
-      button3.disabled = 'disabled';
-      button3.innerHTML = '<i class="fa fa-spinner animate-spin"></i> Processing';
-  
-      var button4 = document.createElement('button');
-      button4.className = 'stop-button bg-red-400 hover:bg-red-600 p-2 rounded text-white status-off status-button';
-      button4.innerHTML = '<i class="fa fa-power-off"></i> Stop';
-  
-      var div3 = document.createElement('div');
-      div3.className = 'w-4/12 flex justify-center items-center';
-  
-      var picture = document.createElement('picture');
-  
-      var source = document.createElement('source');
-      source.srcset = data[i].image_url + ' 1x';
-  
-      var image = document.createElement('img');
-      image.src = 'index_files/no-server-placeholder.png';
-      image.alt = 'placeholder';
-      image.onerror = function() {
-          onError.call(this);
-      };
-  
-      picture.appendChild(source);
-      picture.appendChild(image);
-  
-      div2.appendChild(span1);
-      div2.appendChild(span2);
-      div2.appendChild(span3);
-      div2.appendChild(br1);
-      div2.appendChild(br2);
-      div2.appendChild(button1);
-      div2.appendChild(button2);
-      div2.appendChild(button3);
-      div2.appendChild(button4);
-  
-      div1.appendChild(div2);
-      div1.appendChild(div3);
-  
-      link.appendChild(heading);
-      link.appendChild(div1);
-  
-      article.appendChild(link);
-      article.appendChild(picture);
-  
-      cardList.appendChild(article);
+        var article = document.createElement('article');
+        article.className = 'active';
+        article.setAttribute('data-id', data[i].id);
+    
+        var link = document.createElement('a');
+        link.href = 'https://navlab.ericroy.net/server/' + data[i].id + '/edit';
+    
+        var heading = document.createElement('h2');
+        heading.className = 'p-2';
+        heading.textContent = data[i].name;
+    
+        var div1 = document.createElement('div');
+        div1.className = 'flex';
+    
+        var div2 = document.createElement('div');
+        div2.className = 'w-8/12';
+    
+        var span1 = document.createElement('span');
+        span1.className = 'state-off text-red-500 text-xl';
+        span1.innerHTML = '<i class="fa fa-times"></i> Off';
+    
+        var span2 = document.createElement('span');
+        span2.className = 'state-processing text-gray-500 text-xl';
+        span2.innerHTML = '<i class="fa fa-spinner animate-spin"></i> ...';
+    
+        var span3 = document.createElement('span');
+        span3.className = 'state-on text-green-500 text-xl';
+        span3.innerHTML = '<i class="fa fa-check"></i> ' + data[i].status;
+    
+        var br1 = document.createElement('br');
+        var br2 = document.createElement('br');
+    
+        var button1 = document.createElement('button');
+        button1.className = 'start-button bg-green-400 hover:bg-green-600 p-2 rounded text-white status-off status-button';
+        button1.innerHTML = '<i class="fa fa-power-off"></i> Start';
+    
+        var button2 = document.createElement('button');
+        button2.className = 'cancel-button bg-gray-400 hover:bg-gray-600 p-2 rounded text-white status-off status-button';
+        button2.innerHTML = '<i class="fa fa-spinner animate-spin"></i> Cancel';
+    
+        var button3 = document.createElement('button');
+        button3.className = 'processing-button bg-gray-400 p-2 rounded text-white status-off status-button cursor-not-allowed';
+        button3.disabled = 'disabled';
+        button3.innerHTML = '<i class="fa fa-spinner animate-spin"></i> Processing';
+    
+        var button4 = document.createElement('button');
+        button4.className = 'stop-button bg-red-400 hover:bg-red-600 p-2 rounded text-white status-off status-button';
+        button4.innerHTML = '<i class="fa fa-power-off"></i> Stop';
+    
+        var div3 = document.createElement('div');
+        div3.className = 'w-1/12 flex justify-center items-center';
+    
+        var picture = document.createElement('picture');
+    
+        var source = document.createElement('source');
+        source.srcset = data[i].image_url;
+    
+        var image = document.createElement('img');
+        image.src = './img/no-server-placeholder.png';
+        image.alt = 'placeholder';
+        image.onerror = function() {
+            onError.call(this);
+        };
+    
+        picture.appendChild(source);
+        picture.appendChild(image);
+        div3.appendChild(picture);
+
+        div2.appendChild(span1);
+        div2.appendChild(span2);
+        div2.appendChild(span3);
+        div2.appendChild(br1);
+        div2.appendChild(br2);
+        div2.appendChild(button1);
+        div2.appendChild(button2);
+        div2.appendChild(button3);
+        div2.appendChild(button4);
+    
+        div1.appendChild(div2);
+        div1.appendChild(div3);
+    
+        link.appendChild(heading);
+        link.appendChild(div1);
+    
+        article.appendChild(link);
+        article.appendChild(picture);
+    
+        cardList.appendChild(article);
       }
   
       cardList.innerHTML += `
